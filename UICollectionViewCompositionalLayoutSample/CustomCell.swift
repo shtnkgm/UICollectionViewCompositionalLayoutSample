2604e8adb57a396f9d9289bc28fdf4bc8feb1cff
shtnkgm <nakagami.shota@gmail.com>
2020-02-18 20:08:55 +0900

    fix

---
+++ UICollectionViewCompositionalLayoutSample/CustomCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCell.swift
+//  UICollectionViewCompositionalLayoutSample
+//
+//  Created by Shota Nakagami on 2020/02/18.
+//  Copyright © 2020 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func set(text: String) -> Self {
+        label.text = text
+        return self
+    }
+}
--- UICollectionViewCompositionalLayoutSample/ViewController.swift
+++ UICollectionViewCompositionalLayoutSample/ViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setCollectionView()
     }
+    
+    private func setCollectionView() {
+        
+        let subitem = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1 / 3),
+                heightDimension: .fractionalHeight(1)
+            )
+        )
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            ),
+            subitem: subitem,
+            count: 3
+        )
+        group.interItemSpacing = .fixed(1)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 32
+        
+        let compositionalLayout = UICollectionViewCompositionalLayout(section: section)
+        collectionView.collectionViewLayout = compositionalLayout
+        
+        let nib = UINib(nibName: "CustomCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "CustomCell")
+        collectionView.dataSource = self
+    }
+}
 
-
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return (collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell).set(text: "\(indexPath.section)-\(indexPath.row)")
+    }
 }
 
