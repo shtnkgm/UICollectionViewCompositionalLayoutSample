ec3f7b44f2c0f0469216219598daaf4d3be3706f
snakagam <snakagam@yahoo-corp.jp>
2020-02-18 19:19:08 +0900

    Initial Commit

---
+++ UICollectionViewCompositionalLayoutSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewCompositionalLayoutSample
+//
+//  Created by Shota Nakagami on 2020/02/18.
+//  Copyright © 2020 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
